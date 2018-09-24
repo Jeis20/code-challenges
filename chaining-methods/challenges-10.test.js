60764dd85fe8af4adfdcfe3ef42aa1a659ab60a0
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-09-24 06:23:28 -0700

    challenge 3 completed

--- chaining-methods/challenges-10.test.js
+++ chaining-methods/challenges-10.test.js
@@ -47,6 +47,9 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  return input.map(element => {
+    return element.filter(elementf => elementf % 5 === 0).map(elementM => Math.pow(2, elementM));
+  });
 };
 
 /*------------------------------------------------------------------------------------------------
