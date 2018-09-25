8819e035f75dff7f13add6613a3f0ec72aab811b
Joey Eisenzimmer <joey.eisenzimmer@gmail.com>
2018-09-25 08:45:52 -0700

    working on challenge 3; didn't complete 3 or 4

--- regular-expressions-part-2/challenges-11.test.js
+++ regular-expressions-part-2/challenges-11.test.js
@@ -45,6 +45,7 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 
 const validateEmail = (email) => {
   // Solution code here...
+  return email.search(/^\w+\.)
 };
 
 /*------------------------------------------------------------------------------------------------
